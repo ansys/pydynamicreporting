bd93ab341b54d1a49fbe87d51e6ef03ef3677659
viseshrp <viseshrprasad@gmail.com>
2024-01-03 15:17:07 -0500

    remove ceireports app config

--- src/ansys/dynamicreporting/core/internals/ceireports/contrib/staticfiles/apps.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.staticfiles.apps import StaticFilesConfig
-
-
-class NexusStaticFilesConfig(StaticFilesConfig):
-    # custom ignore list
-    ignore_patterns = StaticFilesConfig.ignore_patterns + ["*.py", "*.md", "*.txt", "LICENSE*", "*.sh"]
