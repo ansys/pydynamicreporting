07c2185b803a9faa92c87f940ed9b65d15b038d9
viseshrp <viseshrprasad@gmail.com>
2024-06-18 11:19:08 -0400

    go back to 24R1

--- src/ansys/dynamicreporting/core/__init__.py
+++ src/ansys/dynamicreporting/core/__init__.py
@@ -8,9 +8,9 @@ except ModuleNotFoundError:  # pragma: no cover
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
 VERSION = __version__
-DEFAULT_ANSYS_VERSION = "242"
+DEFAULT_ANSYS_VERSION = "241"
 
-ansys_version = "2024R2"
+ansys_version = "2024R1"
 
 # Ansys version number that this release is associated with
 __ansys_version__ = DEFAULT_ANSYS_VERSION
