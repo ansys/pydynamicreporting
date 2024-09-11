190747a2cdc029044f443f29022d88c49e1b85d0
viseshrp <viseshrprasad@gmail.com>
2024-09-11 21:00:55 +0530

    allow debug to be specified as kwarg

--- src/ansys/dynamicreporting/core/serverless/adr.py
+++ src/ansys/dynamicreporting/core/serverless/adr.py
@@ -32,6 +32,7 @@ class ADR:
         opts: dict = None,
         request: HttpRequest = None,
         logfile: str = None,
+        debug: bool = None,
     ) -> None:
         self._db_directory = None
         self._media_directory = None
@@ -67,6 +68,13 @@ class ADR:
         else:
             if "CEI_NEXUS_LOCAL_STATIC_DIR" in os.environ:
                 self._static_directory = self._check_dir(os.environ["CEI_NEXUS_LOCAL_STATIC_DIR"])
+
+        if debug is not None:
+            self._debug = debug
+            os.environ["CEI_NEXUS_DEBUG"] = str(int(debug))
+        else:
+            if "CEI_NEXUS_DEBUG" in os.environ:
+                self._debug = bool(int(os.environ["CEI_NEXUS_DEBUG"]))
 
         self._request = request  # passed when used in the context of a webserver.
         self._session = None
