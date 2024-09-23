05994987cdfbdb78b18b57e3f375bd1f689656f5
U-ANSYS\mgalvagn <marina.galvagni@ansys.com>
2024-09-23 15:06:57 -0700

    revert "Fix for syntax"
    This reverts commit d1ccdae0578e2b79f262fec7ff2356f0485f8ae9.

--- test_cleanup.py
+++ test_cleanup.py
@@ -16,7 +16,8 @@ dir_list.extend(
         "tests/test_data/webfonts/",
         "htmltest/",
         "htmltest_again/",
-        "sameport/" "sameport_again/",
+        "sameport/"
+        "sameport_again/"
     ]
 )
 dir_list.append("tests/test_data/create_delete/")
