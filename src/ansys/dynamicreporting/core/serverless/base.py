a03bd11816b7757b09b06fb66b685dcc22a2d54a
viseshrp <viseshrprasad@gmail.com>
2024-10-04 14:07:07 +0530

    fix from_db relation check

--- src/ansys/dynamicreporting/core/serverless/base.py
+++ src/ansys/dynamicreporting/core/serverless/base.py
@@ -218,6 +218,10 @@ class BaseModel(metaclass=BaseMeta):
                 property_fields.append(name)
         return tuple(property_fields) + cls._get_field_names()
 
+    @property
+    def saved(self):
+        return self._saved
+
     @classmethod
     def from_db(cls, orm_instance, parent=None):
         cls_fields = dict(cls._get_field_names(with_types=True, include_private=True))
@@ -242,7 +246,7 @@ class BaseModel(metaclass=BaseMeta):
                     type_ = cls._cls_registry[field_type]
                 else:
                     type_ = field_type
-                if issubclass(type_, cls):
+                if issubclass(cls, type_):  # same hierarchy means there is a parent-child relation
                     value = parent
                 else:
                     value = type_.from_db(value)
@@ -280,28 +284,29 @@ class BaseModel(metaclass=BaseMeta):
         obj._saved = True
         return obj
 
-    @property
-    def saved(self):
-        return self._saved
-
     @handle_field_errors
     def save(self, **kwargs):
         cls_fields = self._get_all_field_names()
         model_fields = self._get_orm_field_names(self._orm_instance)
         for field_ in cls_fields:
-            if field_ in model_fields:
-                value = getattr(self, field_, None)
-                if value is not None:
-                    if isinstance(value, list):
-                        obj_list = []
-                        for obj in value:
-                            obj_list.append(obj._orm_instance)
-                        if obj_list:
-                            getattr(self._orm_instance, field_).add(*obj_list)
-                    else:
-                        if isinstance(value, BaseModel):
-                            value = value._orm_instance.__class__.objects.get(guid=value.guid)
-                        setattr(self._orm_instance, field_, value)
+            if field_ not in model_fields:
+                continue
+            value = getattr(self, field_, None)
+            if value is None:
+                continue
+            # todo: handle template parent and children saving.
+            if isinstance(value, list):
+                obj_list = []
+                for obj in value:
+                    obj_list.append(obj._orm_instance)
+                if obj_list:
+                    getattr(self._orm_instance, field_).add(*obj_list)
+            elif isinstance(value, BaseModel):
+                # todo: handle absence during get()
+                value = value._orm_instance.__class__.objects.using(
+                    kwargs.get("using", "default")
+                ).get(guid=value.guid)
+                setattr(self._orm_instance, field_, value)
         self._orm_instance.save(**kwargs)
         self._saved = True
 
