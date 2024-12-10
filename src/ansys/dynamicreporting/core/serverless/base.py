42be0586b9b701954b47e00d0aa20f6ff5fe1c3b
viseshrp <viseshrprasad@gmail.com>
2024-12-09 19:19:34 -0500

    add missing methods for Template, Layout, Generator
    Update template.py

--- src/ansys/dynamicreporting/core/serverless/base.py
+++ src/ansys/dynamicreporting/core/serverless/base.py
@@ -145,7 +145,7 @@ class BaseModel(metaclass=BaseMeta):
     )  # tracks the corresponding ORM instance
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__}: {self}>"
+        return f"<{self.__class__.__name__}: {self.guid}>"
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__} object {self.guid}"
