f52b8dcc4b3d688a3921a1182a5355f845464d00
phchen <phillip.chen@ansys.com>
2024-12-19 01:47:05 -0500

    add test

--- tests/test_item.py
+++ tests/test_item.py
@@ -358,6 +358,29 @@ def test_create_3d_surface(adr_service_create) -> bool:
     adr_service_create.stop()
     assert len(new_table_items) == (len(table_items) + 1)
 
+@pytest.mark.ado_test
+def test_create_polar_plot(adr_service_create) -> bool:
+    _ = adr_service_create.start(
+        create_db=True,
+        exit_on_close=True,
+        delete_db=True,
+    )
+    filter_str = "A|i_type|cont|table"
+    table_items = adr_service_create.query(query_type="Item", filter=filter_str)
+    my_table = adr_service_create.create_item()
+    my_table.item_table = np.array([
+        ['-180', '-135', '-90', '-45', '0', '45', '90', '135', '180'],
+        [8.2, 7.3, 10.6, 5.6, 5.9, 9.1, 2.4, 1.6, 4.8],
+    ], dtype="|S20")
+    my_table.format = "floatdot0"
+    my_table.plot = "polar plot"
+    my_table.xaxis = 0
+    my_table.format_row = "str"
+    my_table.labels_row = ["theta", "r"]
+    new_table_items = adr_service_create.query(query_type="Item", filter=filter_str)
+    adr_service_create.stop()
+    assert len(new_table_items) == (len(table_items) + 1)
+
 
 def test_set_tags(adr_service_query) -> bool:
     success = False
