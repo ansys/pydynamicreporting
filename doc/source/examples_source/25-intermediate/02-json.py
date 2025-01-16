1490b218a475fd5dc7fdfc0f50fc3b274f7654a3
Yuanrui Zhang <yuanrui.zhang@ansys.com>
2025-01-16 11:30:56 -0500

    Init

---
+++ doc/source/examples_source/25-intermediate/02-json.py
@@ -0,0 +1,16 @@
+"""
+.. _ref_json:
+
+JSON export / import
+==================
+
+Tagging is an important feature of Ansys Dynamic Reporting. Adding tags to items
+allows the database to organize them and the templates to create reports in
+a precise and effective manner. This example shows how to add, modify, query,
+and delete tags on items.
+
+.. note::
+
+   This example assumes that you have a local Ansys installation.
+
+"""
