6935752b74f25beb729396cbcda80645a3c6a9e1
viseshrp <viseshrprasad@gmail.com>
2025-02-24 18:50:55 -0500

    add item sls tests

--- tests/serverless/test_adr.py
+++ tests/serverless/test_adr.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 from ansys.dynamicreporting.core.serverless import ADR
@@ -105,3 +107,347 @@ def test_set_default_dataset(adr_serverless):
 def test_set_default_dataset_no_dataset(adr_serverless):
     with pytest.raises(TypeError, match="Must be an instance of type 'Dataset'"):
         adr_serverless.set_default_dataset(None)
+
+
+@pytest.mark.ado_test
+def test_create_html(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import HTML
+
+    # HTML item
+    item_html = (
+        "<h1>Heading 1</h1>"
+        "<h2>Heading 2</h2>"
+        "<h3>Heading 3</h3>"
+        "<h4>Heading 4</h4>"
+        "<h5>Heading 5</h5>"
+        "Two breaks below"
+        "<br><br />"
+        "<h6>Heading 6 (& one break below)</h6>"
+        "<br>"
+        "The end"
+    )
+    intro_html = adr_serverless.create_item(
+        HTML,
+        name="intro_html",
+        content=item_html,
+        source="sls-test",
+    )
+    assert HTML.get(name="intro_html").guid == intro_html.guid
+
+
+@pytest.mark.ado_test
+def test_set_tags(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import HTML
+
+    intro_html = HTML.get(name="intro_html")
+    intro_html.set_tags("dp=dp227 section=intro")
+    intro_html.save()
+
+    assert "section=intro" in HTML.get(guid=intro_html.guid).tags
+
+
+@pytest.mark.ado_test
+def test_get_tags(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import HTML
+
+    intro_html = HTML.get(name="intro_html")
+    assert "dp=dp227" in intro_html.get_tags()
+
+
+@pytest.mark.ado_test
+def test_edit_html(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import HTML
+
+    intro_html = HTML.get(name="intro_html")
+    intro_html.content = (
+        "<h1>Heading 1</h1>"
+        "<h2>Heading 2</h2>"
+        "<h3>Heading 3</h3>"
+        "<h4>Heading 4</h4>"
+        "<h5>Heading 5</h5>"
+        "Two breaks below"
+        "<br><br />"
+        "<h6>Heading 6 (& one break below)</h6>"
+        "<br>"
+    )
+    intro_html.save()
+
+    assert "end" not in HTML.get(guid=intro_html.guid).content
+
+
+@pytest.mark.ado_test
+def test_add_rem_tag(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import HTML
+
+    intro_html = HTML.get(name="intro_html")
+    intro_html.add_tag("pptx_slide_title", "headers and breaks")
+    intro_html.save()
+    intro_html.rem_tag("pptx_slide_title")
+    intro_html.save()
+
+    assert "pptx_slide_title" not in HTML.get(guid=intro_html.guid).get_tags()
+
+
+@pytest.mark.ado_test
+def test_create_html_bad_content(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import HTML
+
+    with pytest.raises(ValueError):  # must be valid HTML
+        adr_serverless.create_item(HTML, name="empty_html", content="lololol")
+
+
+@pytest.mark.ado_test
+def test_create_string(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import String
+
+    # string
+    item_text = "This section describes the settings for the simulation: initial conditions, solver settings, and such."
+    intro_text = adr_serverless.create_item(
+        String,
+        name="intro_text",
+        content=item_text,
+        tags="dp=dp227 section=intro",
+        source="sls-test",
+    )
+
+    assert String.get(name="intro_text").guid == intro_text.guid
+
+
+@pytest.mark.ado_test
+def test_create_string_no_content(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import String
+
+    with pytest.raises(TypeError):  # must be str
+        adr_serverless.create_item(String, name="empty_text", content=None)
+
+
+@pytest.mark.ado_test
+def test_create_anim(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import Animation
+
+    # anim
+    anim = adr_serverless.create_item(
+        Animation,
+        name="intro_anim",
+        content=str(Path(__file__).parent / "test_data" / "movie.mp4"),
+        tags="dp=dp227 section=data",
+        source="sls-test",
+        sequence=1,
+    )
+
+    assert Animation.get(name="intro_anim").guid == anim.guid
+
+
+@pytest.mark.ado_test
+def test_create_anim_no_content(adr_serverless):  # content is None
+    from ansys.dynamicreporting.core.serverless import Animation
+
+    with pytest.raises(TypeError):
+        adr_serverless.create_item(Animation, name="empty_anim", content=None)
+
+
+@pytest.mark.ado_test
+def test_create_anim_no_file(adr_serverless):  # file does not exist
+    from ansys.dynamicreporting.core.serverless import Animation
+
+    with pytest.raises(ValueError):
+        adr_serverless.create_item(
+            Animation,
+            name="bad_anim",
+            content=str(Path(__file__).parent / "test_data" / "lolol.mp4"),
+        )
+
+
+@pytest.mark.ado_test
+def test_create_anim_bad_format(adr_serverless):  # file is not a video
+    from ansys.dynamicreporting.core.serverless import Animation
+
+    with pytest.raises(ValueError):
+        adr_serverless.create_item(
+            Animation,
+            name="bad_anim",
+            content=str(Path(__file__).parent / "test_data" / "scene.avz"),
+        )
+
+
+@pytest.mark.ado_test
+def test_create_file(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import File
+
+    # file
+    file = adr_serverless.create_item(
+        File,
+        name="intro_file",
+        content=str(Path(__file__).parent / "test_data" / "input.pptx"),
+        tags="dp=dp227 section=data",
+        source="sls-test",
+        sequence=1,
+    )
+
+    assert File.get(name="intro_file").guid == file.guid
+
+
+@pytest.mark.ado_test
+def test_create_file_csf(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import File
+
+    # csf
+    intro_csf = adr_serverless.create_item(
+        File,
+        name="intro_csf",
+        content=str(Path(__file__).parent / "test_data" / "scene1.csf"),
+        tags="dp=dp227 section=data",
+        source="sls-test",
+        sequence=1,
+    )
+
+    assert File.get(name="intro_csf").guid == intro_csf.guid
+
+
+@pytest.mark.ado_test
+def test_create_file_ens(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import File
+
+    # ens
+    intro_ens = adr_serverless.create_item(
+        File,
+        name="intro_ens",
+        content=r"test_data/scene2.ens",
+        tags="dp=dp227 section=data",
+        source="sls-test",
+        sequence=1,
+    )
+
+    assert File.get(name="intro_ens").guid == intro_ens.guid
+
+
+@pytest.mark.ado_test
+def test_create_file_evsn(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import File
+
+    # evsn
+    intro_evsn = adr_serverless.create_item(
+        File,
+        name="intro_evsn",
+        content=r"test_data/scenario.evsn",
+        tags="dp=dp227 section=data",
+        source="sls-test",
+        sequence=1,
+    )
+
+    assert File.get(name="intro_evsn").guid == intro_evsn.guid
+
+
+@pytest.mark.ado_test
+def test_create_image(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import Image
+
+    # image
+    intro_image = adr_serverless.create_item(
+        Image,
+        name="intro_image",
+        content=r"test_data/nexus_logo.png",
+        tags="dp=dp227 section=data",
+        source="sls-test",
+    )
+
+    assert Image.get(name="intro_image").guid == intro_image.guid
+
+
+@pytest.mark.ado_test
+def test_create_enhanced_image(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import Image
+
+    # enhanced image
+    intro_enhanced_image = adr_serverless.create_item(
+        Image,
+        name="intro_enhanced_image",
+        content=r"test_data/case.tif",
+        tags="dp=dp227 section=data",
+        source="sls-test",
+    )
+
+    assert Image.get(name="intro_enhanced_image").guid == intro_enhanced_image.guid
+
+
+@pytest.mark.ado_test
+def test_create_scene(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import Scene
+
+    # scene
+    scene = adr_serverless.create_item(
+        Scene,
+        name="intro_scene",
+        content=r"test_data/scene.avz",
+        session=adr_serverless.session,  # random test
+        dataset=adr_serverless.dataset,
+        tags="dp=dp227 section=data",
+        source="sls-test",
+    )
+
+    assert Scene.get(name="intro_scene").guid == scene.guid
+
+
+@pytest.mark.ado_test
+def test_create_table(adr_serverless):
+    from random import random as r
+
+    import numpy as np
+
+    from ansys.dynamicreporting.core.serverless import Table
+
+    # table
+    ics = []
+    ips = []
+    zet = []
+    for i in range(30):
+        ics.append(i / 5.0)
+        ips.append(np.sin((i + 6 * 0) * np.pi / 10.0) + r() * 0.1)
+        zet.append(np.cos((i + 6 * 0) * np.pi / 10.0) + r() * 0.1)
+
+    intro_table = adr_serverless.create_item(
+        Table,
+        name="intro_table",
+        content=np.array([ics, ips, zet], dtype="|S20"),
+        tags="dp=0 type=hex8",
+        source="sls-test",
+    )
+
+    intro_table.labels_row = ["X", "Sin", "Cos"]
+    intro_table.set_tags("dp=dp227 section=data")
+    intro_table.plot = "line"
+    intro_table.xaxis = "X"
+    intro_table.yaxis = ["Sin", "Cos"]
+    intro_table.xaxis_format = "floatdot0"
+    intro_table.yaxis_format = "floatdot1"
+    intro_table.ytitle = "Values"
+    intro_table.xtitle = "X"
+
+    intro_table.save()
+
+    assert Table.get(name="intro_table").guid == intro_table.guid
+
+
+@pytest.mark.ado_test
+def test_create_tree(adr_serverless):
+    from ansys.dynamicreporting.core.serverless import Tree
+
+    # tree
+    tree_content = [
+        {"key": "root", "name": "Solver", "value": "My Solver"},
+        {"key": "root", "name": "Number cells", "value": 10e6},
+        {"key": "root", "name": "Mesh Size", "value": "1.0 mm^3"},
+        {"key": "root", "name": "Mesh Type", "value": "Hex8"},
+    ]
+    # alternative way of creation
+    tree = adr_serverless.create_item(
+        Tree,
+        name="intro_tree",
+        content=tree_content,
+        tags="dp=dp227 section=data",
+        session=adr_serverless.session,
+        dataset=adr_serverless.dataset,
+        source="sls-test",
+    )
+
+    assert Tree.get(name="intro_tree").guid == tree.guid
