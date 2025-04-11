ef3770085e4ab14c77bcbef2502f98eee078bc8a
James Derrick <jamesgordonderrick@gmail.com>
2025-04-11 15:43:29 +0100

    Update doc/source/examples_source/00-basic/06-explore-plot-properties.py
    Co-authored-by: JennaPaikowsky <98607744+JennaPaikowsky@users.noreply.github.com>

--- doc/source/examples_source/00-basic/06-explore-plot-properties.py
+++ doc/source/examples_source/00-basic/06-explore-plot-properties.py
@@ -12,7 +12,7 @@ properties available.
 .. note::
    This example assumes that you have a local Ansys installation.
 
-Initially we must create and start a session, as per other examples.
+Initially, create and start a session as per other examples.
 
 """
 
