f80fd5e787cece1226ecbb4fd751740b70b18929
viseshrp <viseshrprasad@gmail.com>
2025-04-14 14:21:23 -0400

    add media dir to fixture

--- tests/serverless/conftest.py
+++ tests/serverless/conftest.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 import pytest
 
 from ansys.dynamicreporting.core.constants import DOCKER_DEV_REPO_URL
-from ansys.dynamicreporting.core.serverless import ADR, Item, Template
+from ansys.dynamicreporting.core.serverless import ADR
 
 
 @pytest.fixture(scope="session")
@@ -14,13 +14,17 @@ def adr_serverless(pytestconfig: pytest.Config) -> ADR:
     base_dir = Path(__file__).parent / "test_data"
     local_db = base_dir / f"auto_delete_{uuid4().hex}"
     static_dir = base_dir / "static"
+    media_dir = base_dir / "media"
+
     static_dir.mkdir(exist_ok=True)
+    media_dir.mkdir(exist_ok=True)
 
     if use_local:
         adr = ADR(
             ansys_installation=pytestconfig.getoption("install_path"),
             db_directory=local_db,
             static_directory=static_dir,
+            media_directory=media_dir,
             media_url="/media1/",
             static_url="/static2/",
         )
@@ -30,12 +34,13 @@ def adr_serverless(pytestconfig: pytest.Config) -> ADR:
             docker_image=DOCKER_DEV_REPO_URL,
             db_directory=local_db,
             static_directory=static_dir,
+            media_directory=media_dir,
             media_url="/media1/",
             static_url="/static2/",
         )
+
     adr.setup(collect_static=True)
 
     yield adr
 
-    # Cleanup
     adr.close()
