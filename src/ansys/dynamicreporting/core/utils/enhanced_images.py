8ca531e8da7bc1aec87449666fb8b33efd5548d0
Yuanrui Zhang <yuanrui.zhang@ansys.com>
2025-06-12 11:03:40 -0400

    Support multiple var pages in a TIFF image

--- src/ansys/dynamicreporting/core/utils/enhanced_images.py
+++ src/ansys/dynamicreporting/core/utils/enhanced_images.py
@@ -66,6 +66,17 @@ if HAS_VTK and HAS_DPF:  # pragma: no cover
             Leave it unfilled if it is a scalar variable.
         """
         _generate_enhanced_image(model, var_field, part_name, output_file_name, rotation, component)
+        
+    def generate_enhanced_image_as_tiff_multi_var_pages(
+        model: dpf.Model,
+        var_fields: list[Tuple[dpf.Field, str]],  # a list of dpf.Field and component
+        part_name: str,
+        output_file_name: str,
+        rotation: Tuple[float, float, float] = (0.0, 0.0, 0.0),
+    ):
+        _generate_enhanced_image_multi_var_pages(
+            model, var_fields, part_name, output_file_name, rotation
+        )
 
     def generate_enhanced_image_in_memory(
         model: dpf.Model,
@@ -369,11 +380,55 @@ if HAS_VTK and HAS_DPF:  # pragma: no cover
         np_buffer = np_buffer.reshape(height, width)
         return np_buffer
 
+    # def _form_enhanced_image(
+    #     json_data: Dict,
+    #     rgb_buffer: np.ndarray,
+    #     pick_buffer: np.ndarray,
+    #     var_buffer: np.ndarray,
+    #     output: Union[str, io.BytesIO],
+    # ) -> None:
+    #     """
+    #     A helper function. Build up an enhanced image and output to either a TIFF file on
+    #     disk or to a byte buffer.
+
+    #     Parameters
+    #     ----------
+    #     json_data: Dict
+    #         A dictionary that contains "parts" and "variables" sections.
+    #     rgb_buffer: np.ndarray
+    #         An int8 buffer with RGB values. Its dimension is [height, width, 3].
+    #     pick_buffer: np.ndarray
+    #         An int8 buffer with pick data. Its dimension is [height, width, 3].
+    #     var_buffer: np.ndarray
+    #         An float32 buffer with variable data. Its dimension is [height, width].
+    #     output: Union[str, io.BytesIo]
+    #         Specify the output to be either a file name or a byte buffer.
+    #     """
+    #     # json_data as metadata called image_description to store in the enhanced image
+    #     image_description = json.dumps(json_data)
+
+    #     # Create 3 images for each page
+    #     rgb_image = Image.fromarray(rgb_buffer, mode="RGB")
+    #     pick_image = Image.fromarray(pick_buffer, mode="RGBA")
+    #     var_image = Image.fromarray(var_buffer, mode="F")
+
+    #     # Set up the metadata
+    #     tiffinfo = TiffImagePlugin.ImageFileDirectory_v2()
+    #     tiffinfo[TiffImagePlugin.IMAGEDESCRIPTION] = image_description
+
+    #     rgb_image.save(
+    #         output,
+    #         format="TIFF",
+    #         save_all=True,
+    #         append_images=[pick_image, var_image],
+    #         tiffinfo=tiffinfo,
+    #     )
+        
     def _form_enhanced_image(
         json_data: Dict,
         rgb_buffer: np.ndarray,
         pick_buffer: np.ndarray,
-        var_buffer: np.ndarray,
+        var_buffers: list[np.ndarray],
         output: Union[str, io.BytesIO],
     ) -> None:
         """
@@ -388,30 +443,133 @@ if HAS_VTK and HAS_DPF:  # pragma: no cover
             An int8 buffer with RGB values. Its dimension is [height, width, 3].
         pick_buffer: np.ndarray
             An int8 buffer with pick data. Its dimension is [height, width, 3].
-        var_buffer: np.ndarray
-            An float32 buffer with variable data. Its dimension is [height, width].
+        var_buffers: list[np.ndarray]
+            A list of float32 buffers with variable data. Each buffer's dimension is [height, width].
         output: Union[str, io.BytesIo]
             Specify the output to be either a file name or a byte buffer.
         """
         # json_data as metadata called image_description to store in the enhanced image
         image_description = json.dumps(json_data)
 
-        # Create 3 images for each page
+        # Create the RGB and pick images
         rgb_image = Image.fromarray(rgb_buffer, mode="RGB")
         pick_image = Image.fromarray(pick_buffer, mode="RGBA")
-        var_image = Image.fromarray(var_buffer, mode="F")
+
+        # Convert variable buffers to images
+        var_images = [Image.fromarray(var_buffer, mode="F") for var_buffer in var_buffers]
 
         # Set up the metadata
         tiffinfo = TiffImagePlugin.ImageFileDirectory_v2()
         tiffinfo[TiffImagePlugin.IMAGEDESCRIPTION] = image_description
 
+        # Save the TIFF file with all images
         rgb_image.save(
             output,
             format="TIFF",
             save_all=True,
-            append_images=[pick_image, var_image],
+            append_images=[pick_image] + var_images,
             tiffinfo=tiffinfo,
         )
+
+    def _generate_enhanced_image_multi_var_pages(
+        model: dpf.Model,
+        var_fields: list[Tuple[dpf.Field, str]], # a list of dpf.Field and component
+        part_name: str,
+        output: Union[str, io.BytesIO],
+        rotation: Tuple[float, float, float] = (0.0, 0.0, 0.0),
+    ) -> None:
+        count = 0
+        rgb_buffer = None
+        pick_buffer = None
+        var_buffers = []
+        json_data_variables = []
+        for var_field, component in var_fields:
+            is_vector_var = var_field.data.ndim > 1
+            if is_vector_var:  # if it is a vector variable
+                if component is None:
+                    print(
+                        "Error when generating an enhanced image: The field data is vector variable. "
+                        "Currently, we do not fully support vector variables. "
+                        "Please specify which component you want to plot upon, 'X', 'Y' or 'Z'"
+                    )
+                    return
+                if component not in ("X", "Y", "Z"):
+                    print(
+                        "Error when generating an enhanced image: "
+                        "The parameter 'component' only takes 'X', 'Y' or 'Z'"
+                    )
+                    return
+
+            # Get components for metadata
+            var_unit: str = var_field.unit
+            var_name = var_field.name + "" if component is None else f"[{component}]" # TODO: var_name should be user input
+            dpf_unit_system = model.metadata.result_info.unit_system_name
+            unit_system_to_name = dpf_unit_system.split(":", 1)[0]
+            meshed_region = model.metadata.meshed_region  # Whole mesh region
+            json_data_variables.append(
+                {
+                    "name": var_name,
+                    "id": "3456",  # Todo: optimize hardcoded part ID
+                    "pal_id": "1",  # colorby_var_int,
+                    "unit_dims": "",
+                    "unit_system_to_name": unit_system_to_name,
+                    "unit_label": var_unit,
+                }
+            )
+
+            # Convert DPF to a pyvista UnstructuredGrid, which inherits from vtk
+            grid = vtk_helper.dpf_mesh_to_vtk(meshed_region)
+            # Add variable data
+            grid = vtk_helper.append_field_to_grid(var_field, meshed_region, grid, var_name)
+
+            geometry_filter = vtk.vtkGeometryFilter()
+            geometry_filter.SetInputData(grid)
+            geometry_filter.Update()
+            poly_data = geometry_filter.GetOutput()
+
+            # Extract the required component from the vector data
+            if is_vector_var:
+                if component == "X":
+                    col = 0
+                elif component == "Y":
+                    col = 1
+                else:
+                    col = 2
+
+                point_data = poly_data.GetPointData()
+                vtk_array = point_data.GetArray(var_name)
+                var_array = vtk_to_numpy(vtk_array)
+                idx_array = var_array[:, col]
+
+                trimmed_vtk_array = numpy_to_vtk(idx_array, deep=True)
+                trimmed_vtk_array.SetName(var_name)
+                point_data.RemoveArray(var_name)
+                point_data.AddArray(trimmed_vtk_array)
+            
+            renderer, render_window = _setup_render_routine(poly_data, rotation)
+            if count == 0:
+                rgb_buffer = _get_rgb_value(render_window)
+                # Assign the pick data
+                _add_pick_data(poly_data, 3456)
+                pick_buffer = _render_pick_data(poly_data, renderer, render_window)
+            
+            var_buffer = _render_var_data(poly_data, renderer, render_window, var_name)
+            var_buffers.append(var_buffer)
+            count += 1
+            
+        json_data = {
+            "parts": [
+                {
+                    "name": part_name,
+                    "id": "3456",  # Todo: optimize hardcoded part ID
+                    "colorby_var": "1.0",  # colorby_var
+                }
+            ],
+            "variables": json_data_variables,
+        }
+
+        _form_enhanced_image(json_data, rgb_buffer, pick_buffer, var_buffers, output)
+
 
     def _generate_enhanced_image(
         model: dpf.Model,
@@ -420,9 +578,9 @@ if HAS_VTK and HAS_DPF:  # pragma: no cover
         output: Union[str, io.BytesIO],
         rotation: Tuple[float, float, float] = (0.0, 0.0, 0.0),
         component: str = None,
-    ) -> Tuple[Dict, np.ndarray, np.ndarray, np.ndarray]:
+    ) -> None:
         """
-        Esstential helper function for DPF inputs. Generate json metadata, rgb buffer, pick
+        Essential helper function for DPF inputs. Generate json metadata, rgb buffer, pick
         data buffer and variable data buffer from a DPF model object and a DPF field object.
 
         Parameters
@@ -439,11 +597,6 @@ if HAS_VTK and HAS_DPF:  # pragma: no cover
         component: str
             For vector variable, specify which component to plot, 'X', 'Y' or 'Z'
             Leave it unfilled if it is a scalar variable.
-
-        Returns
-        -------
-        Tuple[Dict, np.ndarray, np.ndarray, np.ndarray]
-            A tuple of JSON metadata, rgb buffer, pick data buffer and variable data buffer
         """
         # Todo: vector data support: is_scalar_data = var_data.ndim == 1
         is_vector_var = var_field.data.ndim > 1
@@ -535,4 +688,4 @@ if HAS_VTK and HAS_DPF:  # pragma: no cover
             ],
         }
 
-        _form_enhanced_image(json_data, rgb_buffer, pick_buffer, var_buffer, output)
+        _form_enhanced_image(json_data, rgb_buffer, pick_buffer, [var_buffer], output)
