1541357e6d5f3fa0d25d5867ffc59b47aa4cb4d0
Yuanrui Zhang <yuanrui.zhang@ansys.com>
2025-06-17 16:03:08 -0400

    Update fixture to see if codecov can see the function body

--- tests/test_enhanced_images.py
+++ tests/test_enhanced_images.py
@@ -41,50 +41,46 @@ def setup_dpf_inmem_generation():
     image.close()
 
 
-@pytest.fixture(params=[setup_dpf_tiff_generation, setup_dpf_inmem_generation])
+@pytest.fixture(params=["tiff", "inmem"])
 def setup_generation_flow(request):
-    try:
-        return next(request.param())
-    # The exception is raised when DPF server is not found due to unset env var
-    # In this case, we return None in order to skip the test.
-    except ValueError:
-        return None
+    if request.param == "tiff":
+        for image in setup_dpf_tiff_generation():
+            yield image
+    else:
+        for image in setup_dpf_inmem_generation():
+            yield image
 
 
 @pytest.mark.ado_test
 def test_basic_format(setup_generation_flow):
     image = setup_generation_flow
-    if image is None:
-        assert True
-    else:
-        image.seek(0)
-        result = ru.is_enhanced(image)
-        assert result is not None
+    assert image is not None
+    image.seek(0)
+    result = ru.is_enhanced(image)
+    assert result is not None
 
 
 @pytest.mark.ado_test
 def test_image_description(setup_generation_flow):
     image = setup_generation_flow
-    if image is None:
-        assert True
-    else:
-        image.seek(0)
-        metadata_dict = {TAGS[key]: image.tag[key] for key in image.tag_v2}
-        image_description = json.loads(metadata_dict["ImageDescription"][0])
-        part_info = image_description["parts"][0]
-        var_info = image_description["variables"][0]
+    assert image is not None
+    image.seek(0)
+    metadata_dict = {TAGS[key]: image.tag[key] for key in image.tag_v2}
+    image_description = json.loads(metadata_dict["ImageDescription"][0])
+    part_info = image_description["parts"][0]
+    var_info = image_description["variables"][0]
 
-        assert (
-            part_info["name"] == "DPF Sample"
-            and part_info["id"] == "3456"
-            and part_info["colorby_var"] == "1.0"
-        )
+    assert (
+        part_info["name"] == "DPF Sample"
+        and part_info["id"] == "3456"
+        and part_info["colorby_var"] == "1.0"
+    )
 
-        assert (
-            var_info["name"] == "var"
-            and var_info["id"] == "3456"
-            and var_info["pal_id"] == "1"
-            and var_info["unit_dims"] == ""
-            and var_info["unit_system_to_name"] == "MKS"
-            and var_info["unit_label"] == "V"
-        )
+    assert (
+        var_info["name"] == "var"
+        and var_info["id"] == "3456"
+        and var_info["pal_id"] == "1"
+        and var_info["unit_dims"] == ""
+        and var_info["unit_system_to_name"] == "MKS"
+        and var_info["unit_label"] == "V"
+    )
