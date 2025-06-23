8ea73a8111a0925b93cbf12f50c86194ea4a64e3
Yuanrui Zhang <yuanrui.zhang@ansys.com>
2025-06-23 12:12:52 -0400

    Remove redundant code

--- tests/test_enhanced_images.py
+++ tests/test_enhanced_images.py
@@ -13,12 +13,7 @@ from ansys.dynamicreporting.core.utils import report_utils as ru
 
 def get_dpf_model_field_example():
     file_path = examples.find_electric_therm()
-    if not os.path.exists(file_path):
-        print("No no no")
-        raise FileNotFoundError(f"Example file not found at {file_path}")
-
-    data_path = examples.find_electric_therm()
-    server_data_path = dpf.upload_file_in_tmp_folder(data_path)
+    server_data_path = dpf.upload_file_in_tmp_folder(file_path)
     model = dpf.Model(server_data_path)
 
     results = model.results
