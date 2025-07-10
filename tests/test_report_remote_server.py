ba7a7f5581b5b995c3a9751f6eebac3ca78a06cc
Yuanrui Zhang <yuanrui.zhang@ansys.com>
2025-07-10 13:20:17 -0400

    Fix a type

--- tests/test_report_remote_server.py
+++ tests/test_report_remote_server.py
@@ -673,7 +673,7 @@ def test_check_templates_parent_name(adr_service_create) -> bool:
         e.TemplateEditorJSONLoadingError,
         match=(
             "The loaded JSON file has an invalid template name: 'WRONG_NAME' "
-            "that does not have the correct name convection under the key: 'parent' of 'Template_1'\n"
+            "that does not have the correct name convention under the key: 'parent' of 'Template_1'\n"
             "Please note that the naming convention is 'Template_{NONE_NEGATIVE_NUMBER}'"
         ),
     ):
@@ -731,7 +731,7 @@ def test_check_templates_children_name(adr_service_create) -> bool:
         e.TemplateEditorJSONLoadingError,
         match=(
             "The loaded JSON file has an invalid template name: 'WRONG_NAME' "
-            "that does not have the correct name convection under the key: 'children' of 'Template_0'\n"
+            "that does not have the correct name convention under the key: 'children' of 'Template_0'\n"
             "Please note that the naming convention is 'Template_{NONE_NEGATIVE_NUMBER}'"
         ),
     ):
@@ -1222,7 +1222,7 @@ def test_get_layout_types(adr_service_create) -> None:
     server = adr_service_create.serverobj
     layout_types = server.get_layout_types()
     expected_layout_types = LAYOUT_TYPES
-    assert isinstance(layout_types, list)
+    assert isinstance(layout_types, tuple)
     assert layout_types == expected_layout_types
     assert all(isinstance(layout_type, str) for layout_type in layout_types)
 
@@ -1232,7 +1232,7 @@ def test_get_generator_types(adr_service_create) -> None:
     server = adr_service_create.serverobj
     generator_types = server.get_generator_types()
     expected_generator_types = GENERATOR_TYPES
-    assert isinstance(generator_types, list)
+    assert isinstance(generator_types, tuple)
     assert generator_types == expected_generator_types
     assert all(isinstance(generator_type, str) for generator_type in generator_types)
 
@@ -1242,6 +1242,6 @@ def test_get_report_types(adr_service_create) -> None:
     server = adr_service_create.serverobj
     report_types = server.get_report_types()
     expected_report_types = REPORT_TYPES
-    assert isinstance(report_types, list)
+    assert isinstance(report_types, tuple)
     assert report_types == expected_report_types
     assert all(isinstance(report_type, str) for report_type in report_types)
