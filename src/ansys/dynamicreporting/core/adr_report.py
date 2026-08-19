60029d091f0f43b51903bcb714474d1185f1e338
Visesh Rajendraprasad <viseshrp@users.noreply.github.com>
2026-08-19 16:20:50 -0400

    add back version to html export

--- src/ansys/dynamicreporting/core/adr_report.py
+++ src/ansys/dynamicreporting/core/adr_report.py
@@ -765,6 +765,7 @@ class Report:
                 item_filter=item_filter,
                 filename=filename,
                 no_inline_files=no_inline_files,
+                ansys_version=self.service._ansys_version,
             )
             success = True
         except Exception as e:  # pragma: no cover
